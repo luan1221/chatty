fe906f62c5f308d025c8f2587b6da9649dbd30fa
luan1221 <lssa@cin.ufpe.br>
2021-04-20 13:09:47 -0300

    refatoracao da rota de POST /settings e criacao do controller de settings

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,8 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from './repositories/SettingsRepository';
+import SettingsController from './controllers/SettingsController';
 
 const router = Router();
 
-router.post('/settings', async (req, res) => {
-  const { chat, username } = req.body;
-  const settingsRepository = getCustomRepository(SettingsRepository);
-  
-  const settings = settingsRepository.create({
-      chat,
-    username
-  });
-
-  await settingsRepository.save(settings);
-
-  return res.json(settings);
-});
+router.post('/settings', SettingsController.create);
 
 export { router };
