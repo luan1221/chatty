c21b098a06e176643833383942d47571d1de8ec7
luan1221 <lssa@cin.ufpe.br>
2021-04-21 15:43:40 -0300

    instanciacao do controller de users pelo router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import SettingsController from './controllers/SettingsController';
+import UsersController from './controllers/UsersController';
 
 const router = Router();
 
 router.post('/settings', SettingsController.create);
+
+router.post('/users', UsersController.create);
 
 export { router };
