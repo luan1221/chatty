eddf1294aff083b09b97ef4fc4836260aaff7e20
luan1221 <lssa@cin.ufpe.br>
2021-04-22 18:26:23 -0300

    configuracoes iniciais do websocket usando socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express from "express";
-import { router } from './routes';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 import './database';
+import { router } from './routes';
 
 const app = express();
+
+const http = createServer(app);
+const io = new Server(http);
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket: Socket) => {
+  console.log('Connected', socket.id);
+});
+
+http.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}!`);
 });
