2b11c0b8822dce7d095cec20d94b2db1ea6404b6
luan1221 <lssa@cin.ufpe.br>
2021-04-22 18:33:12 -0300

    configuracao para uso do cliente para testes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
+import path from 'path';
 
 import './database';
 import { router } from './routes';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 const http = createServer(app);
 const io = new Server(http);
