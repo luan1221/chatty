176f6cf50de214f2abf13ef846a3a99f8ef8f0fd
luan1221 <lssa@cin.ufpe.br>
2021-04-24 12:25:15 -0300

    funcao que estabelece a comunicacao entre o admin e o cliente no html

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,8 @@
   const socket = io();
+  let connectionsUsers = [];
 
   socket.on('admin-list-all-users', (connections) => {
+    connectionsUsers = connections;
     document.getElementById('list_users').innerHTML = "";
     let template = document.getElementById('template').innerHTML;
     connections.forEach(connection => {
@@ -9,5 +11,15 @@
         id: connection.socketId
       });
       document.getElementById('list_users').innerHTML += rendered;
-    })
+    });
   });
+
+function call(id) {
+  const connection = connectionsUsers.find(connection => connection.socketId === id);
+  const template = document.getElementById('admin_template').innerHTML;
+  const rendered = Mustache.render(template, {
+    email: connection.user.email,
+    id: connection.userId
+  });
+  document.getElementById('supports').innerHTML += rendered;
+}
